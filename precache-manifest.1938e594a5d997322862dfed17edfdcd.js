045331091469e9ac7236c981aab2d1eb8274ade4
JAksa Malisic <jaksa.malisic@gmail.com>
2019-11-15 03:43:07 +0100

    Updates

---
+++ precache-manifest.1938e594a5d997322862dfed17edfdcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3d5a095054db061390d97d47b67469",
+    "url": "/jaksm/qr-scanner/index.html"
+  },
+  {
+    "revision": "8d5a923df0e4f20abe0a",
+    "url": "/jaksm/qr-scanner/static/css/2.c5b05437.chunk.css"
+  },
+  {
+    "revision": "52ddf947f37442ea1671",
+    "url": "/jaksm/qr-scanner/static/css/main.8bc4ddfd.chunk.css"
+  },
+  {
+    "revision": "8d5a923df0e4f20abe0a",
+    "url": "/jaksm/qr-scanner/static/js/2.5e6e8c76.chunk.js"
+  },
+  {
+    "revision": "52ddf947f37442ea1671",
+    "url": "/jaksm/qr-scanner/static/js/main.d0d6fff1.chunk.js"
+  },
+  {
+    "revision": "11efe64c4c056862834d",
+    "url": "/jaksm/qr-scanner/static/js/runtime-main.d34db889.js"
+  }
+]);
